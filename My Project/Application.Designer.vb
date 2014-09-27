c91ec4d7475b7b750581b9f5aa6b2ea2ad48a7bf
Woomber <realidadvirtualwm@gmail.com>
2014-09-27 16:22:13 -0500

    Splash screen simple

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.Episteme.StartUI
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.Episteme.Splash
+        End Sub
     End Class
 End Namespace
---
+++ Splash.vb
@@ -0,0 +1,20 @@
+﻿Public NotInheritable Class Splash
+
+    Private Sub Splash_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If My.Application.Info.Title <> "" Then
+            ApplicationTitle.Text = My.Application.Info.Title
+        Else
+            ApplicationTitle.Text = System.IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        End If
+
+        Version.Text = System.String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor, My.Application.Info.Version.Build, My.Application.Info.Version.Revision)
+
+        Copyright.Text = My.Application.Info.Copyright
+
+        Description.Text = My.Application.Info.Description
+
+        Company.Text = My.Application.Info.CompanyName
+
+    End Sub
+End Class
